cbc4f679b478a26a2d272f0a1288a2c6e424aaf0
Sarah Chen <sarahlc888@gmail.com>
2018-08-02 10:30:50 -0500

    bsearch fixed

--- 0.00-Templates/src/ModBinSearch2.java
+++ 0.00-Templates/src/ModBinSearch2.java
@@ -7,8 +7,7 @@ public class ModBinSearch2 {
 	
 	public static void main(String args[]) throws IOException {
 		int[] arr1 = {1, 2, 6, 8};
-		System.out.println(leastAbove(arr1, 5));
-		
+		System.out.println(bSearch(arr1, 80));
 	}
 	public static int greatestBelow(int[] arr, long val) {
 		// returns greatest i <= val
@@ -44,6 +43,27 @@ public class ModBinSearch2 {
 			}
 		}
 		if (arr[lo] >= val) return lo; // make sure lo works (catches rare exceptions)
+		return -1;
+	}
+	public static int bSearch(int[] arr, long val) {
+		// returns smallest i >= val
+		int lo = 0;
+		int hi = arr.length-1;
+		int mid = 0;
+		
+		while (lo <= hi) {
+			mid = (lo + hi)/2; // no +1 because you want hi to become lo in 1st if
+//			System.out.println("lo: " + lo + " hi: " + hi + " mid: " + mid);
+
+			if (arr[mid] > val) { // if mid is in range
+				hi = mid-1;
+			} else if (arr[mid] < val){ // mid is not in range
+				lo = mid+1;
+			} else { // == val
+				break;
+			}
+		}
+		if (arr[mid] == val) return mid; // make sure it works
 		return -1;
 	}
 	
