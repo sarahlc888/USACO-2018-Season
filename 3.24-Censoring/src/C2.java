6316ecf9c4dbbcd5e17a962495f35f0e99eb383b
Sarah Chen <sarahlc888@gmail.com>
2018-08-23 17:47:10 -0500

    idea 1 8/15 cases

--- 3.24-Censoring/src/C2.java
+++ 3.24-Censoring/src/C2.java
@@ -5,9 +5,9 @@ import java.util.*;
  * Problem 2. Censoring (Gold)
  * 8/23
  * 
- * 7/15 correct
+ * 8/15 correct
  */
-public class Censoring {
+public class C2 {
 	static ArrayList<ArrayList<Integer>> adj;
 	static int[] par;
 	static char[] nodes;
@@ -31,6 +31,7 @@ public class Censoring {
 		//System.out.println(S);
 		//System.out.println(Arrays.toString(C));
 		
+
 		// Tree
 		adj = new ArrayList<ArrayList<Integer>>();
 		for (int i = 0; i < totLen; i++) 
@@ -49,21 +50,27 @@ public class Censoring {
 			// in the tree, end of where w sub, where to start adding
 			int stub = exists(sub); 
 			
-//			//System.out.println("word: " + w);
-//			//System.out.println("  ind: " + ind);
-//			//System.out.println("  stub: " + stub);
+//			System.out.println("word: " + w);
+//			System.out.println("  ind: " + ind);
+//			System.out.println("  stub: " + stub);
 			
 			for (int j = ind+1; j < w.length(); j++) {
 				char cur = w.charAt(j);
 //				//System.out.println("    cur: " + cur + " stub: " + stub);
 				nodes[id] = cur; // create node
-				adj.get(stub).add(id); // add to tree
+				
+//				System.out.println(adj.get(stub) + "  " + cur);
+//				if (adj.get(stub).size() > 0) System.out.println(nodes[adj.get(stub).get(0)]);
+				
+				int addind = leastAbove(adj.get(stub), nodes[id]);
+				adj.get(stub).add(addind, id); // add to tree
 				par[id] = stub; // mark parent
 				stub = id; // update stub
 				id++;
 			}
+//			System.out.println("adj: " + adj);
+//			System.out.println("nodes: " + Arrays.toString(nodes));
 		}
-
 
 		//System.out.println("adj: " + adj);
 		//System.out.println("nodes: " + Arrays.toString(nodes));
@@ -142,6 +149,8 @@ public class Censoring {
 				i = wordStart-longest+1;
 				i = Math.max(0, i);
 				//System.out.println("new i: " + i);
+				toVisit = new LinkedList<State>(); // next round of toVisit
+				toVisit2 = new LinkedList<State>(); // next round of toVisit
 			}
 			
 			
@@ -156,6 +165,62 @@ public class Censoring {
 		pw.println(S);
 		pw.close();
 	}
+	public static int leastAbove(ArrayList<Integer> arr, char val) {
+		// returns smallest i where nodes[i] >= char
+		int lo = 0;
+		int hi = arr.size()-1;
+		
+		if (arr.size() == 0) return 0;
+		if (nodes[arr.get(hi)] < val) return arr.size();
+		
+		
+		while (lo < hi) {
+			int mid = (lo + hi)/2; // no +1 because you want hi to become lo in 1st if
+			char c = nodes[arr.get(mid)];
+
+			if (c >= val) { // if mid is in range
+				hi = mid;
+			} else { // mid is not in range
+				lo = mid + 1;
+			}
+		}
+//		System.out.println("lo: " + lo);
+//		System.out.println(nodes[arr.get(lo)]);
+//		System.out.println(val);
+		if (nodes[arr.get(lo)] >= val) return lo; // make sure lo works (catches rare exceptions)
+		
+		return arr.size();
+	}
+	public static int bSearch(ArrayList<Integer> arr, char val) {
+		// returns i where nodes[i] = char
+		int lo = 0;
+		int hi = arr.size()-1;
+		int mid = 0;
+		
+		if (arr.size() == 0) return -1;
+		
+		if (nodes[arr.get(lo)] > val || nodes[arr.get(hi)] < val) {
+			return -1;
+		}
+		
+		while (lo <= hi) {
+			mid = (lo + hi)/2; // no +1 because you want hi to become lo in 1st if
+
+			char c = nodes[arr.get(mid)];
+			
+			if (c > val) {
+				hi = mid-1;
+			} else if (arr.get(mid) < val) {
+				lo = mid+1;
+			} else { // == val
+				break;
+			}
+		}
+		
+		
+		if (nodes[arr.get(mid)] == val) return mid; // make sure it works
+		return -1;
+	}
 	public static int exists(String s) {
 		// if string s exists in the current tree, return the end of path there
 		// -1 means doesn't exist
@@ -163,16 +228,15 @@ public class Censoring {
 		
 		for (int i = 0; i < s.length(); i++) {
 			boolean found = false;
-			for (int j = 0; j < adj.get(cur).size(); j++) {
-				int neigh = adj.get(cur).get(j);
-				char c = nodes[neigh];
-				if (c == s.charAt(i)) {
-//					//System.out.println("c is in string: " + c);
-					cur = neigh;
-					found = true;
-					break;
-				}
+			
+			int ind = bSearch(adj.get(cur), s.charAt(i));
+			
+			if (ind != -1) {
+				int neighNode = adj.get(cur).get(ind);
+				cur = neighNode;
+				found = true;
 			}
+			
 			if (!found) return -1;
 		}
 		return cur;
@@ -183,17 +247,13 @@ public class Censoring {
 		int cur = 0;
 		
 		for (int i = 0; i < s.length(); i++) {
-			boolean found = false;
-			for (int j = 0; j < adj.get(cur).size(); j++) {
-				int neigh = adj.get(cur).get(j);
-				char c = nodes[neigh];
-				if (c == s.charAt(i)) {
-					cur = neigh;
-					found = true;
-					break;
-				}
+			int ind = bSearch(adj.get(cur), s.charAt(i));
+			
+			if (ind != -1) {
+				cur = adj.get(cur).get(ind);
+			} else {
+				return i-1;
 			}
-			if (!found) return i-1;
 		}
 		return s.length()-1;
 	}
